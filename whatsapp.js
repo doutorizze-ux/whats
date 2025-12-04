d0cafed7a637fb4d168a791e2611f656527f8ccb
User <user@example.com>
2025-12-04 08:19:32 -0300

    fix: update browser signature to Ubuntu/Chrome for better compatibility

--- whatsapp.js
+++ whatsapp.js
@@ -111,7 +111,7 @@ const createSession = async (sessionId, res = null, options = { usePairingCode: 
         msgRetryCounterCache,
         generateHighQualityLinkPreview: true,
         getMessage,
-        browser: ['Whatsender', 'Windows', '10.0.0'],
+        browser: ['Ubuntu', 'Chrome', '20.0.04'],
         retryRequestDelayMs: 2000,
         connectTimeoutMs: 60000,
         keepAliveIntervalMs: 10000,
